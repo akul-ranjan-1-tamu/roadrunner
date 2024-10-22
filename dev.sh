56c736bd9e079559e7760b0e7d3f774509579356
Caleb Elizondo <calebelizondo@tamu.edu>
2024-10-22 15:42:56 -0500

    added quiet flag

--- dev.sh
+++ dev.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+
+#NOTE: this script might not work depending on your OS or dev env. This works on Windows
+
+QUIET=false
+if [[ "$1" == "--quiet" ]]; then
+    QUIET=true
+fi
+
+
 #start backend
 cd backend
 pip install -r requirements.txt
 python3 app.py &
 
-
+#start frontend
 cd ../frontend
-npm run start
+if [ "$QUIET" = true ]; then
+    npm run start > /dev/null 2>&1
+else
+    npm run start
+fi
