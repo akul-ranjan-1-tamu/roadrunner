f4bbf3bb4cd141a3823cb7c8db40c679ecadd667
Akul Ranjan <128449155+AkulCodes@users.noreply.github.com>
2024-10-28 17:00:15 -0500

    added dial types

--- frontend/src/widgets/utils/getWidgetComponent.tsx
+++ frontend/src/widgets/utils/getWidgetComponent.tsx
@@ -1,4 +1,5 @@
 import BasicDisplay, { BasicDisplayConfig } from "../basic-display/BasicDisplay";
+import Dial, { DialConfig } from "../dial/dial";
 import { WIDGET_TYPE, WidgetConfig } from "../types";
 
 
@@ -7,6 +8,9 @@ export const getWidgetComponent: (config: WidgetConfig, selected: boolean) => JS
     
     if (widgetType === WIDGET_TYPE.BASIC_DISPLAY) {
         return <BasicDisplay selected={selected} config={config as BasicDisplayConfig} />
+    }
+    else if (widgetType === WIDGET_TYPE.DIAL) {
+        return <Dial selected={selected} config={config as DialConfig} />
     }
     else {
         return <BasicDisplay selected={selected} config={config as BasicDisplayConfig} />
